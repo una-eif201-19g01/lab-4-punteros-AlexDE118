d3859590eecf459176ba608c7340bf5b5044403f
alexanderdittel <alexanderdittel11@gmail.com>
2019-08-23 09:17:31 -0600

    commit2

--- Empleado.h
+++ Empleado.h
@@ -17,8 +17,28 @@
 #ifndef EMPLEADOPUNTEROS_EMPLEADO_H
 #define EMPLEADOPUNTEROS_EMPLEADO_H
 
-
 #include <string>
+#include <iostream>
 
+using namespace std;
+
+const int IDMIN = 1;
+const int IDMAX = 100;
+const float SALARIO_BASE = 800000;
+
+class Empleado {
+//atributos
+protected:
+	string *nombre;
+	int *id;
+	float *salario;
+	bool *flagRevision;
+//Metodo
+
+public:
+	Empleado();
+private:
+
+};
 
 #endif //EMPLEADOPUNTEROS_EMPLEADO_H
